a757a0c7f50aabc2566303e97c3f8d25dea123dc
NemoNomi <162903221+NemoNomi@users.noreply.github.com>
2025-01-11 23:15:46 +0100

    challenge

---
+++ prototype_2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnCooldown = 2f;
+
+    private float timeSinceLastSpawn = 0f;
+
+    void Update()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && timeSinceLastSpawn >= spawnCooldown)
+        {
+
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            timeSinceLastSpawn = 0f;
+        }
+    }
+}
